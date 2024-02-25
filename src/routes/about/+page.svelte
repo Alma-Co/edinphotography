b8ec3417e91170877ce50e8db687de86bd65e478
Edith <eddith@proton.me>
2024-02-25 21:20:33 +0100

    config to src/lib

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,37 +1,5 @@
 <script>
-	import image1 from '$lib/images/test1.jpg';
-	import image2 from '$lib/images/test2.jpg';
-	import image3 from '$lib/images/test3.jpg';
-	import image4 from '$lib/images/test4.jpg';
-	import image5 from '$lib/images/test5.jpg';
 	import Picture from '../Picture.svelte';
-	const images = [
-		{
-			picture: image1,
-			alt: "alt 1",
-			description: "here is a blablabla"
-		},
-		{
-			picture: image2,
-			alt: 'alt 2',
-			description: "here is a blablabla"
-		},
-		{
-			picture: image3,
-			alt: 'alt 3',
-			description: "here is a blablabla"
-		},
-		{
-			picture: image4,
-			alt: 'alt 4',
-			description: "here is a blablabla"
-		},
-		{
-			picture: image5,
-			alt: 'alt 5',
-			description: "here is a blablabla"
-		},
-	]
 </script>
 <svelte:head>
 	<title>About</title>
@@ -39,11 +7,11 @@
 </svelte:head>
 
 <section>
-	<div class="flex flex-row gap-8 flex-wrap">
-		{#each images as object}
-			<Picture src={object.picture} alt={object.alt} />
+	<!-- <div class="flex flex-row gap-8 flex-wrap">
+		{#each array as object}
+			<Picture src={object.image} alt={object.imageAlt} />
 		{/each}
-	</div>
+	</div> -->
 </section>
 
 
