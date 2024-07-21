65432e900e816f8b283adb72b7b0d9a6cd2133c4
Edith <eddith@proton.me>
2024-07-21 20:40:13 +0200

    add flex in colors and shapes

--- src/routes/colors-and-shapes/+page.svelte
+++ src/routes/colors-and-shapes/+page.svelte
@@ -3,9 +3,32 @@
 <script lang="ts">
 
 	import data from '$lib/data/colors-and-shapes.json';
-	import type { Data } from '$lib/models/types.svelte';
-	import GridImage from '../GridImage.svelte';
+	import type { Data, ImagesBlock } from '$lib/models/types.svelte';
+	import Gallery from '../../components/Gallery.svelte';
+	import GridImage from '../../components/GridImage.svelte';
+	import Picture from '../../components/Picture.svelte';
 	const { components } = data as Data;
+	
+	const allImages = components.flatMap(component => 
+  		component.imagesBlock.flatMap(block => 
+    		block.images.map((image, index) => {
+    			const imageName = image.image.split('/').pop()?.split('.')[0];
+    				return {
+    				  src: image.image,
+    				  alt: imageName || `${index}`,
+    				  id: imageName || `${index}`
+    				};
+    			})
+ 			 )
+		);
+
+
+	let defaultModal = false;
+	let pictureId = "";
+	const openModal = (id:string) => {
+    	pictureId = id;
+    	defaultModal = true;
+  	}
 
 </script>
 <svelte:head>
@@ -16,13 +39,29 @@
 <section>
 	{#each components as component}
 		{#if component.type === 'grid'}
-			<div class="grid grid-cols-4 gap-3">
+			<div class="grid grid-cols-2 md:grid-cols-4 gap-3">
 				{#each component.imagesBlock as block}
-					<GridImage {block} />
+					<GridImage {openModal} bind:pictureId bind:defaultModal {block} />
+				{/each}
+			</div>
+		{/if}
+		{#if component.type === 'flex'}
+			<div class="flex justify-center flex-wrap gap-3">
+				{#each component.imagesBlock as { images }, i}
+					{#each images as image, j}
+						<Picture {openModal} 
+                			id={image.image.split('/').pop()?.split('.')[0]} 
+                			imageRatio={'border-white border-4 rounded-sm object-cover w-[350px]'} 
+                			containerClass={'flex'} 
+                			src={image.image} 
+                			alt={image.imageAlt || null} 
+            			/>
+					{/each}
 				{/each}
 			</div>
 		{/if}
 	{/each}
+	<Gallery gallery={allImages} bind:defaultModal bind:pictureId />
 </section>
 
 
