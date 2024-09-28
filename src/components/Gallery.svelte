043796ef0dd9e0b0a4250a8e37f6c364de62b6e3
Edith <eddith@proton.me>
2024-09-28 22:45:06 +0200

    swap on mobile in gallery

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -1,115 +1,173 @@
 <script lang="ts">
-    import { IconChevronRight, IconChevronLeft, IconX } from '@tabler/icons-svelte';
-    export let gallery: {src: string, alt: string, id: string}[];
-    export let pictureId: string;
-    export let defaultModal: boolean;
-    
-    $: getPositionInArrayById = gallery.find((image) => image.id === pictureId);
-    $: getIndexInArrayById = gallery.findIndex((image) => image.id === pictureId);
+	import { IconChevronRight, IconChevronLeft, IconX } from '@tabler/icons-svelte';
+	import { onMount } from 'svelte';
+	export let gallery: { src: string; alt: string; id: string }[];
+	export let pictureId: string;
+	export let defaultModal: boolean;
+
+	let startX = 0;
+	let endX = 0;
+
+	$: getPositionInArrayById = gallery.find((image) => image.id === pictureId);
+	$: getIndexInArrayById = gallery.findIndex((image) => image.id === pictureId);
+
+	const handleTouchStart = (event: any) => {
+		startX = event.touches[0].clientX;
+	};
+
+	const handleTouchMove = (event: any) => {
+		endX = event.touches[0].clientX;
+	};
+
+	const handleTouchEnd = () => {
+		if (startX - endX > 50) {
+			// Swipe left
+			if (getIndexInArrayById < gallery.length - 1) {
+				pictureId = gallery[getIndexInArrayById + 1].id;
+			}
+		} else if (endX - startX > 50) {
+			// Swipe right
+			if (getIndexInArrayById > 0) {
+				pictureId = gallery[getIndexInArrayById - 1].id;
+			}
+		}
+	};
+
+	onMount(() => {
+		const modalContent = document.querySelector('.modal-content');
+
+		if (!modalContent) return;
+		modalContent.addEventListener('touchstart', handleTouchStart);
+		modalContent.addEventListener('touchmove', handleTouchMove);
+		modalContent.addEventListener('touchend', handleTouchEnd);
+
+		return () => {
+			modalContent.removeEventListener('touchstart', handleTouchStart);
+			modalContent.removeEventListener('touchmove', handleTouchMove);
+			modalContent.removeEventListener('touchend', handleTouchEnd);
+		};
+	});
 </script>
 
-<div class={defaultModal ? 'modal-open': 'modal'}>
+<div class={defaultModal ? 'modal-open' : 'modal'}>
 	<div class="modal-content">
-     
-  		{#if getPositionInArrayById}
-      {#if gallery[getIndexInArrayById].id !== gallery[0].id}
-       <button on:click={() => (pictureId = gallery[getIndexInArrayById - 1].id)} class="buttons previous">
-        <IconChevronLeft size={45} stroke={1} />
-      </button>
-      {/if}
-  		<img class="modal-image" src={gallery[getIndexInArrayById].src} alt={gallery[getIndexInArrayById].alt} />
-       {#if gallery[getIndexInArrayById].id !== gallery[gallery.length - 1].id}
-       <button on:click={() => (pictureId = gallery[getIndexInArrayById + 1].id)} class="buttons next">
-        <IconChevronRight size={45} stroke={1} />
-      </button>
-      {/if}
-  		{/if}
-     
-  		<button on:click={() => (defaultModal = false)} class="close"> <IconX size={30} stroke={2} /></button>
+		{#if getPositionInArrayById}
+			{#if gallery[getIndexInArrayById].id !== gallery[0].id}
+				<button
+					on:click={() => (pictureId = gallery[getIndexInArrayById - 1].id)}
+					class="buttons previous"
+				>
+					<IconChevronLeft size={60} stroke={1} />
+				</button>
+			{/if}
+			<img
+				class="modal-image"
+				src={gallery[getIndexInArrayById].src}
+				alt={gallery[getIndexInArrayById].alt}
+			/>
+			{#if gallery[getIndexInArrayById].id !== gallery[gallery.length - 1].id}
+				<button
+					on:click={() => (pictureId = gallery[getIndexInArrayById + 1].id)}
+					class="buttons next"
+				>
+					<IconChevronRight size={60} stroke={1} />
+				</button>
+			{/if}
+		{/if}
+
+		<button on:click={() => (defaultModal = false)} class="close">
+			<IconX size={30} stroke={2} /></button
+		>
 	</div>
 </div>
 
 <style>
-    .modal {
-    	display: none;
-    }
+	.modal {
+		display: none;
+	}
 
-    .modal-open {
-      display: block;
-      position: fixed;
-      z-index: 10;
-      padding-top: 60px;
-      left: 0;
-      top: 0px;
-    	width: 100%;
-      height: 100%;
-      overflow: auto; 
-      background-color: rgb(0,0,0);
-      background-color: rgba(0, 0, 0, 0.644); 
-    }
+	.modal-open {
+		display: block;
+		position: fixed;
+		z-index: 10;
+		left: 0;
+		top: 0px;
+		width: 100%;
+		height: 100%;
+		overflow: auto;
+		background-color: rgb(0, 0, 0);
+		background-color: rgba(0, 0, 0, 0.644);
+	}
 
-    .modal-content {
-      background-color: rgb(0,0,0);
-      margin: auto;
-      padding: 20px;
-      width: 90%;
-      height: 94%;
-      display: flex;
-      justify-content: center;
-      position: relative
-    }
+	.modal-content {
+		background-color: rgb(0, 0, 0);
+		margin: 0;
+		height: 100%;
+		display: flex;
+		justify-content: center;
+		position: relative;
+	}
 
-    .modal-image {
-      max-width: 85%;
-      object-fit: contain;
-    }
+	.modal-image {
+		max-width: 85%;
+		object-fit: contain;
+	}
 
-    .close {
-      color: #625e5e;
-      position: absolute;
-      top: 10px;
-      right: 10px;
-      border: none;
-        background: none;
-        cursor: pointer;
-    }
+	.close {
+		color: #625e5e;
+		position: absolute;
+		top: 10px;
+		right: 10px;
+		border: none;
+		background: none;
+		cursor: pointer;
+	}
 
-    .close:hover,
-    .close:focus {
-      color: #c2bebe;
-      text-decoration: none;
-      cursor: pointer;
-    }
+	.close:hover,
+	.close:focus {
+		color: #c2bebe;
+		text-decoration: none;
+		cursor: pointer;
+	}
 
-    .buttons {
-      color: #625e5e;
-      position: absolute;
-      top: 50%;
-      border: none;
-      background: none;
-      cursor: pointer;
-      &.next {
-        right: 0px;
-      }
-      &.previous {
-        left: 0px;
-      }
-    }
+	.buttons {
+		display: none;
+		color: #625e5e;
+		position: absolute;
+		top: 50%;
+		border: none;
+		background: none;
+		cursor: pointer;
+		&.next {
+			right: 30px;
+		}
+		&.previous {
+			left: 30px;
+		}
+	}
 
-    .buttons:hover {
-      color: #c2bebe;
-      cursor: pointer;
-    }
+	.buttons:hover {
+		color: #c2bebe;
+		cursor: pointer;
+	}
 
-    @media (min-width: 600px) {
-      .modal-content {
-      width: 94%;
-      height: 97%;
-      }
+	@media (min-width: 905px) {
+		.buttons {
+			display: block;
+		}
 
-      .close {
-      top: 20px;
-      }
-    }
+		.modal-open {
+			padding-top: 60px;
+		}
+		.modal-content {
+			width: 100%;
+			height: 97%;
+			padding: 20px;
+		}
 
+		.close {
+			top: 20px;
+			right: 40px;
+		}
+	}
 </style>
