879959ba61fc1ff6427fae5cd9c3fcc8669ade24
Edith <eddith@proton.me>
2024-09-28 22:46:29 +0200

    reshape

--- src/routes/colors-and-shapes/+page.svelte
+++ src/routes/colors-and-shapes/+page.svelte
@@ -1,36 +1,32 @@
-
-
 <script lang="ts">
-
 	import data from '$lib/data/colors-and-shapes.json';
 	import type { Data, ImagesBlock } from '$lib/models/types.svelte';
 	import Gallery from '../../components/Gallery.svelte';
 	import GridImage from '../../components/GridImage.svelte';
 	import Picture from '../../components/Picture.svelte';
 	const { components } = data as Data;
-	
-	const allImages = components.flatMap(component => 
-  		component.imagesBlock.flatMap(block => 
-    		block.images.map((image, index) => {
-    			const imageName = image.image.split('/').pop()?.split('.')[0];
-    				return {
-    				  src: image.image,
-    				  alt: imageName || `${index}`,
-    				  id: imageName || `${index}`
-    				};
-    			})
- 			 )
-		);
 
+	const allImages = components.flatMap((component) =>
+		component.imagesBlock.flatMap((block) =>
+			block.images.map((image, index) => {
+				const imageName = image.image.split('/').pop()?.split('.')[0];
+				return {
+					src: image.image,
+					alt: imageName || `${index}`,
+					id: imageName || `${index}`
+				};
+			})
+		)
+	);
 
 	let defaultModal = false;
-	let pictureId = "";
-	const openModal = (id:string) => {
-    	pictureId = id;
-    	defaultModal = true;
-  	}
-
+	let pictureId = '';
+	const openModal = (id: string) => {
+		pictureId = id;
+		defaultModal = true;
+	};
 </script>
+
 <svelte:head>
 	<title>Colors and shapes</title>
 	<meta name="description" content="colors and shapes pictures" />
@@ -44,10 +40,10 @@
 					<GridImage {openModal} bind:pictureId bind:defaultModal {block} />
 				{/each}
 			</div>
-			{#if component.spacer && component.spacer.padding === "small"}
+			{#if component.spacer && component.spacer.padding === 'small'}
 				<div class="py-8"></div>
 			{/if}
-			{#if component.spacer && component.spacer.padding === "large"}
+			{#if component.spacer && component.spacer.padding === 'large'}
 				<div class="py-14"></div>
 			{/if}
 		{/if}
@@ -55,20 +51,21 @@
 			<div class="flex justify-center flex-wrap gap-3">
 				{#each component.imagesBlock as { images }, i}
 					{#each images as image, j}
-						<Picture {openModal} 
-                			id={image.image.split('/').pop()?.split('.')[0]} 
-                			imageRatio={'border-white border-4 rounded-sm object-cover w-[350px]'} 
-                			containerClass={'flex'} 
-                			src={image.image} 
-                			alt={image.imageAlt || null} 
-            			/>
+						<Picture
+							{openModal}
+							id={image.image.split('/').pop()?.split('.')[0]}
+							imageRatio={'border-white border-4 rounded-sm object-cover w-[350px]'}
+							containerClass={'flex'}
+							src={image.image}
+							alt={image.imageAlt || null}
+						/>
 					{/each}
 				{/each}
 			</div>
-			{#if component.spacer && component.spacer.padding === "small"}
+			{#if component.spacer && component.spacer.padding === 'small'}
 				<div class="py-8"></div>
 			{/if}
-			{#if component.spacer && component.spacer.padding === "large"}
+			{#if component.spacer && component.spacer.padding === 'large'}
 				<div class="py-14"></div>
 			{/if}
 		{/if}
@@ -76,11 +73,9 @@
 	<Gallery gallery={allImages} bind:defaultModal bind:pictureId />
 </section>
 
-
 <style>
 	section {
 		width: 100%;
 		height: 100%;
 	}
-
 </style>
