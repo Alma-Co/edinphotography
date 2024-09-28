1c58d3e50008736575dc5a8e74c91956b2f8f130
Edith <eddith@proton.me>
2024-09-28 22:45:43 +0200

    navbar styles

--- src/components/GridImage.svelte
+++ src/components/GridImage.svelte
@@ -1,58 +1,75 @@
 <script lang="ts">
-    import Picture from './Picture.svelte';
+	import Picture from './Picture.svelte';
 
-    export let block;
+	export let block;
 
-    const imageRatio = 'border-white border-4 rounded-sm object-cover w-full'
+	const imageRatio = 'border-white border-4 rounded-sm object-cover w-full';
 
-    const { images, disposition } = block;	
-    export let defaultModal: boolean;
-    export let pictureId: string;
-    export let openModal: (id:string) => void;
-
-  
+	const { images, disposition } = block;
+	export let defaultModal: boolean;
+	export let pictureId: string;
+	export let openModal: (id: string) => void;
 </script>
 
-{#if disposition === "middle"}
-        {#each images as object, i}
-            <Picture {openModal} 
-                id={object.image.split('/').pop()?.split('.')[0]} 
-                imageRatio={imageRatio} 
-                containerClass={i=== 0 ? `col-start-1 md:col-start-2 flex ${images.length === 1 && "col-span-2"}` : 'flex'} 
-                src={object.image} 
-                alt={object.imageAlt || null} 
-            />
-        {/each}
-{:else if disposition === "line"}
-        {#each images as object, i}
-            <Picture 
-                {openModal} 
-                id={object.image.split('/').pop()?.split('.')[0]} 
-                imageRatio={imageRatio} 
-                containerClass={i=== 0 ? `col-start-1 flex ${images.length === 2 && "col-span-2"}` : `flex ${images.length === 2 && "col-span-2"}`} 
-                src={object.image} 
-                alt={object.imageAlt || null} 
-            />
-        {/each}
-{:else if disposition === "right"}
-        {#each images as object, i}
-            <Picture 
-                {openModal} 
-                id={object.image.split('/').pop()?.split('.')[0]} 
-                imageRatio={imageRatio} 
-                containerClass={i=== 0 ? 'col-start-3 flex' : 'flex'} 
-                src={object.image} 
-                alt={object.imageAlt || null} 
-            />
-        {/each}
-{:else if disposition === "left"}
-        {#each images as object, i}
-            <Picture {openModal} id={object.image.split('/').pop()?.split('.')[0]} imageRatio={imageRatio} containerClass={i=== 0 ? 'col-start-1 flex' : 'flex'} src={object.image} alt={object.imageAlt || null} />
-        {/each}
+{#if disposition === 'middle'}
+	{#each images as object, i}
+		<Picture
+			{openModal}
+			id={object.image.split('/').pop()?.split('.')[0]}
+			{imageRatio}
+			containerClass={i === 0
+				? `col-start-1 md:col-start-2 flex ${images.length === 1 && 'col-span-2'}`
+				: 'flex'}
+			src={object.image}
+			alt={object.imageAlt || null}
+		/>
+	{/each}
+{:else if disposition === 'line'}
+	{#each images as object, i}
+		<Picture
+			{openModal}
+			id={object.image.split('/').pop()?.split('.')[0]}
+			{imageRatio}
+			containerClass={i === 0
+				? `col-start-1 flex ${images.length === 2 && 'col-span-2'}`
+				: `flex ${images.length === 2 && 'col-span-2'}`}
+			src={object.image}
+			alt={object.imageAlt || null}
+		/>
+	{/each}
+{:else if disposition === 'right'}
+	{#each images as object, i}
+		<Picture
+			{openModal}
+			id={object.image.split('/').pop()?.split('.')[0]}
+			{imageRatio}
+			containerClass={i === 0 ? 'col-start-3 flex' : 'flex'}
+			src={object.image}
+			alt={object.imageAlt || null}
+		/>
+	{/each}
+{:else if disposition === 'left'}
+	{#each images as object, i}
+		<Picture
+			{openModal}
+			id={object.image.split('/').pop()?.split('.')[0]}
+			{imageRatio}
+			containerClass={i === 0 ? 'col-start-1 flex' : 'flex'}
+			src={object.image}
+			alt={object.imageAlt || null}
+		/>
+	{/each}
 {:else}
-    {#each images as object}
-            <Picture {openModal} id={object.image.split('/').pop()?.split('.')[0]} imageRatio={imageRatio} containerClass={'flex'} src={object.image} alt={object.imageAlt || null} />
-    {/each}
+	{#each images as object}
+		<Picture
+			{openModal}
+			id={object.image.split('/').pop()?.split('.')[0]}
+			{imageRatio}
+			containerClass={'flex'}
+			src={object.image}
+			alt={object.imageAlt || null}
+		/>
+	{/each}
 {/if}
 
 <style lang="postcss">
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,50 +1,43 @@
-
 <script lang="ts">
 	import { IconMenu, IconX } from '@tabler/icons-svelte';
 	import { page } from '$app/stores';
-	import camera from '$lib/images/camera-love.svg';
+	import lulu from '$lib/images/lulu.svg';
 
 	$: open = false;
 </script>
 
 <header class="relative flex flex-col items-center pb-12">
-	<div class="flex flex-col gap-2 justify-center items-center text-xl mt-8">
-		<h1 class="text-3xl md:text-5xl font-medium">
-			Ed in photography 
-		</h1>
-		<div class="flex items-center gap-2">
-		<span>--</span> 
-		<img class="w-[15px] sm:w-[20px]" src={camera} alt="Camera" />
-		<span>--</span> 
-		</div>
+	<div class="flex flex-col justify-center items-center md:gap-2 text-xl mt-8">
+		<a href="/">
+			<img class="w-[60px] sm:w-[100px]" src={lulu} alt="Camera" />
+		</a>
+		<h1 class="text-1xl md:text-3xl font-medium">Ed in photography</h1>
 	</div>
 
 	<nav class={open ? 'nav-open' : 'hidden'}>
-
 		<ul>
-		<li aria-current={$page.url.pathname === '/colors-and-shapes' ? 'page' : undefined}>
-			<a on:click={() => open = false} href="/colors-and-shapes">Colors and shapes</a>
-		</li>
-		<li aria-current={$page.url.pathname === '/places-and-moments' ? 'page' : undefined}>
-			<a on:click={() => open = false} href="/places-and-moments">Places and moments</a>
-		</li>
-		<li aria-current={$page.url.pathname === '/analog' ? 'page' : undefined}>
-			<a on:click={() => open = false} href="/analog">Analog</a>
-		</li>
-	</ul>
+			<li aria-current={$page.url.pathname === '/colors-and-shapes' ? 'page' : undefined}>
+				<a on:click={() => (open = false)} href="/colors-and-shapes">Colors and shapes</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/places-and-moments' ? 'page' : undefined}>
+				<a on:click={() => (open = false)} href="/places-and-moments">Places and moments</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/analog' ? 'page' : undefined}>
+				<a on:click={() => (open = false)} href="/analog">Analog</a>
+			</li>
+		</ul>
 	</nav>
 
-	<button on:click={() => open = !open}>
-	{#if open}
-        <IconX size={25} stroke={1} />
-    {:else}
-        <IconMenu size={25} stroke={1} />
-    {/if}
+	<button on:click={() => (open = !open)}>
+		{#if open}
+			<IconX size={25} stroke={1} />
+		{:else}
+			<IconMenu size={25} stroke={1} />
+		{/if}
 	</button>
 </header>
 
 <style>
-
 	h1 {
 		font-family: 'Reenie Beanie';
 		/* font-size: 2rem; */
@@ -62,7 +55,6 @@
 		height: 100%;
 	}
 
-
 	ul {
 		position: fixed;
 		top: 0;
@@ -74,7 +66,7 @@
 		justify-content: center;
 		align-items: start;
 		list-style: none;
-		padding: 0 1rem;
+		padding: 10rem 1rem;
 		background-color: black;
 		width: 100%;
 		height: 30%;
@@ -123,6 +115,7 @@
 			position: static;
 			gap: 2rem;
 			padding-top: 2rem;
+			padding-bottom: 0;
 			margin: 0;
 			list-style: none;
 			flex-direction: row;
@@ -132,7 +125,6 @@
 	}
 
 	@media (min-width: 905px) {
-
 		ul {
 			gap: 3.5rem;
 		}
@@ -143,4 +135,3 @@
 		}
 	}
 </style>
-
